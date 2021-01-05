9115de6de52c529d69550795b674cf39c92a1dd2
xande <aleturial8@gmail.com>
2021-01-05 14:59:46 -0300

    movimento-rei

---
+++ Assets/Scripts/Command/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : Command
+{
+    public Movement()
+    {
+    }
+    public override void Execute()
+    {
+
+    }
+
+    public override void Undo()
+    {
+        
+    }
+}
